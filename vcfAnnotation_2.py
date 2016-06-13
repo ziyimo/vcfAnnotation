a7dcd6159c6c6862190bb728c31e69ebf356c45c
ziyimo <ZiyiMo@Ziyis-MacBook-Pro.local>
2016-06-13 16:26:16 +0400

    Command line parser is implemented. The main function of the program is modified to perform the task in the way intended (i.e. the coordinate of the amino acid change of the non- synonymous SNP is matched to the coordinate of the predicted Pfam domains.)

--- vcfAnnotation_2.py
+++ vcfAnnotation_2.py
@@ -1,4 +1,18 @@
 import datetime
+import argparse
+
+message=''
+
+#Implement command line parser
+parser=argparse.ArgumentParser(description='Placeholder for program description') #write a short description!!
+parser.add_argument('fastaDir', type=str, help='Specify the proteome fasta file directory')
+parser.add_argument('pfamDir',type=str,help='Specify the pfam tsv file directory')
+parser.add_argument('vcfDir',type=str,help='Specify the vcf file directory')
+parser.add_argument('-t','--targetDir',type=str,default='',help='Specify the target directory for the updated vcf file and the log file')
+parser.add_argument('-a','--annformat',action='store_true',help='run the program on ann formatted SNPs')
+args=parser.parse_args()
+if args.targetDir != '':
+	args.targetDir+='/'
 
 class VCF:
 	# parsing the vcf file when instantiating a VCF object
@@ -22,13 +36,14 @@ class VCF:
 			newFile.write('\t'.join(SNPinfo)+'\n')
 
 
-
 def main(INFOField):
 
-	'''The funtion takes the INFO field of a SNP (as a string). If the SNPEFF_IMPACT value is MODIFIER, the transcript ID
-	the SNP associates with will be matched with Pfam data (generated from a single tsv file). Two tags are added to the 
-	INFO field, SNPEFF_PFAMID contains the pfam ID of all domains that the SNP can affect, whereas SNPEFF_DOMAINCOOR specifies
-	the coordinates of the domains affected within the transcript. The function returns the updated INFO field (as a string).
+	'''The funtion takes the INFO field of a SNP (as a string). If the SNP is non-synonymous, the coordinate of 
+	the amino acid change is extracted. The transcript ID the SNP associates with will be matched with Pfam data 
+	(generated from a single tsv file). If the amino acid change falls within the coordinate of a pfam domain, 
+	two tags are added to the INFO field, SNPEFF_PFAMID contains the pfam ID of all domains that the SNP can affect, 
+	whereas SNPEFF_DOMAINCOOR specifies	the coordinates of the domains affected within the transcript. 
+	The function returns the updated INFO field (as a string).
 	In addition, a list of all unique transcript ID records in the vcf file is compiled.
 	'''
 
@@ -39,16 +54,20 @@ def main(INFOField):
 		INFODict[a[0]]=a[1]
 	if INFODict['SNPEFF_TRANSCRIPT_ID'] not in vcf_tscptID:
 		vcf_tscptID.append(INFODict['SNPEFF_TRANSCRIPT_ID'])
-	if INFODict['SNPEFF_IMPACT']=='MODIFIER':
-		logStats['ModifierSNPrcd']+=1
-		if INFODict['SNPEFF_TRANSCRIPT_ID'] not in modifierSNP_tscptID:
-			modifierSNP_tscptID.append(INFODict['SNPEFF_TRANSCRIPT_ID'])
+	if INFODict['SNPEFF_EFFECT']=='NON_SYNONYMOUS_CODING':
+		logStats['NonsynSNPrcd']+=1
+		if INFODict['SNPEFF_TRANSCRIPT_ID'] not in nonsynSNP_tscptID:
+			nonsynSNP_tscptID.append(INFODict['SNPEFF_TRANSCRIPT_ID'])
+		pepCoor=int(INFODict['SNPEFF_AMINO_ACID_CHANGE'][1:len(INFODict['SNPEFF_AMINO_ACID_CHANGE'])-1])
+		refPep=INFODict['SNPEFF_AMINO_ACID_CHANGE'][0]
+		if fasta_pepDict[INFODict['SNPEFF_TRANSCRIPT_ID']][pepCoor-1]!=refPep:
+			message=message+'Anomaly detected at entry #'+vcfData.dataFields.index(entry)+' of the vcf file!\n'
 		for entry in pfamData:
-			if entry[0]==INFODict['SNPEFF_TRANSCRIPT_ID']:
+			if entry[0]==INFODict['SNPEFF_TRANSCRIPT_ID'] and int(entry[6])<=pepCoor<=int(entry[7]):
 				if 'SNPEFF_PFAMID' not in INFODict:
 					INFODict['SNPEFF_PFAMID']=entry[4]
 					INFODict['SNPEFF_DOMAINCOOR']=entry[6]+'-'+entry[7]
-					logStats['matchedModifierSNPrcd']+=1
+					logStats['matchedNonsynSNPrcd']+=1
 				else:
 					INFODict['SNPEFF_PFAMID']= INFODict['SNPEFF_PFAMID']+','+entry[4]
 					INFODict['SNPEFF_DOMAINCOOR']= INFODict['SNPEFF_DOMAINCOOR']+','+entry[6]+'-'+entry[7]
@@ -63,19 +82,20 @@ def main(INFOField):
 
 	return ';'.join(newINFOList)
 
-logStats={'ModifierSNPrcd':0,'matchedModifierSNPrcd':0}
+logStats={'NonsynSNPrcd':0,'matchedNonsynSNPrcd':0}
 
-#Readind transcriptID records from proteome fasta file
-fasta_pepDirectory=input('Specify the proteome fasta file directory: ')
-fasta_pep=open(fasta_pepDirectory,'r')
-fasta_pep_tscptID=[]
+#Readind records from proteome fasta file
+fasta_pep=open(args.fastaDir,'r')
+fasta_pepDict={}
 for line in fasta_pep:
 	if line[:1]=='>':
-		fasta_pep_tscptID.append(line[1:].strip())
+		tscptID=line[1:].strip()
+		fasta_pepDict[tscptID]=''
+	else:
+		fasta_pepDict[tscptID]+=line.strip()
 
 #Putting the pfam data in a 2 dimensional list
-pfamTSVDirectory=input('Specify the Pfam tsv file directory: ')
-pfamTSV=open(pfamTSVDirectory,'r')
+pfamTSV=open(args.pfamDir,'r')
 pfamData=[]
 pfam_tscptID=[]
 for line in pfamTSV:
@@ -86,14 +106,13 @@ for line in pfamTSV:
 pfamTSV.close()
 
 #Putting the SNP data into an object -- vcfData
-vcfFileDirectory=input('Specify vcf file directory: ')
-vcfFile=open(vcfFileDirectory, 'r')
+vcfFile=open(args.vcfDir, 'r')
 vcfData=VCF(vcfFile)
 vcfFile.close()
 
 #Performing main task on every entry
 vcf_tscptID=[]
-modifierSNP_tscptID=[]
+nonsynSNP_tscptID=[]
 for entry in vcfData.dataFields:
 	updatedINFO=main(entry[7])
 	entry[7]=updatedINFO
@@ -106,8 +125,7 @@ for line in vcfData.metaInfo:
 		break
 
 #Creating new vcf file
-targetDirectory=input('Specify target directory: ')
-newVCF=open(targetDirectory+'/updated.vcf','w')
+newVCF=open(args.targetDir+'updated.vcf','w')
 vcfData.writeVCFFile(newVCF)
 newVCF.close()
 
@@ -115,17 +133,17 @@ newVCF.close()
 logStats['SNPrcd']=len(vcfData.dataFields)
 logStats['PFAMrcd']=len(pfamData)
 logStats['tscptID_vcf']=len(vcf_tscptID)
-logStats['tscptID_ModifierSNP']=len(modifierSNP_tscptID)
+logStats['tscptID_NonsynSNP']=len(nonsynSNP_tscptID)
 logStats['tscptID_pfam']=len(pfam_tscptID)
-logStats['%matched']=str(logStats['matchedModifierSNPrcd']/logStats['ModifierSNPrcd']*100)+'%'
+logStats['%matched']=str(logStats['matchedNonsynSNPrcd']/logStats['NonsynSNPrcd']*100)+'%'
 
 tscptID_vcf_fasta=0
 for tscptID in vcf_tscptID:
-	if tscptID in fasta_pep_tscptID:
+	if tscptID in fasta_pepDict:
 		tscptID_vcf_fasta+=1
 tscptID_pfam_fasta=0
 for tscptID in pfam_tscptID:
-	if tscptID in fasta_pep_tscptID:
+	if tscptID in fasta_pepDict:
 		tscptID_pfam_fasta+=1
 logStats['vcf/fasta_pep']=str(tscptID_vcf_fasta/len(vcf_tscptID)*100)+'%'
 logStats['pfam/fasta_pep']=str(tscptID_pfam_fasta/len(pfam_tscptID)*100)+'%'
@@ -133,8 +151,9 @@ logStats['pfam/fasta_pep']=str(tscptID_pfam_fasta/len(pfam_tscptID)*100)+'%'
 logtxt=str(datetime.datetime.today())+'\n'
 for stat in list(logStats.items()):
 	logtxt=logtxt+stat[0]+'='+str(stat[1])+'\n'
+logtxt=logtxt+'\n\n'+message
 print (logtxt)
 
-logfile=open(targetDirectory+'/log.txt','w')
+logfile=open(args.targetDir+'log.txt','w')
 logfile.write(logtxt)
 logfile.close()
